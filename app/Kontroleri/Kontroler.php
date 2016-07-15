9d63f25dc1c75cd85173c28327fd39e8f8280553
icoklo <icoklo@foi.hr>
2016-07-15 15:55:50 +0200

    Sloziti reusability u klasi Kontroler
    U klasi Kontroler posloziti reusability funkcije ispisDohvacenihPodataka
    (skratiti ime funkcije) te posloziti da se ta funkcija moze koristiti za
    ispis bilo kakvih podataka (korisnika, postova, itd), a ne kako je sada
    slozena samo za ispis podataka korisnika

--- app/Kontroleri/Kontroler.php
+++ app/Kontroleri/Kontroler.php
@@ -2,9 +2,6 @@
 
 namespace Kontroler;
 
-// napraviti klasu u kojoj ce biti definirana metoda za ispis i koja se koristi u mojoj klasi RadSKorisnicima kako bi se
-// olaksao ispis podataka, da se ne ponavlja stalno isti dio koda, ta funkcija bi trebala primati parametre (podaciZaIspis,poruka,status)
-
 // funkcije unutar klase nazivati po camel case notaciji: imeFunkcije
 // obicne funkcije unutar php skripte nazivati stilom: ime_funkcije
 
@@ -20,14 +17,16 @@ class Kontroler{
 	}
 
 	// funkcija za ispis poruke korisnike, npr. nakon uspjesnog unosa novog korisnika
+	// $status=200 je zapravo postavljena defaultna vrijednost tog parametra
 	protected function ispisiPoruku($poruka,$status=200){
 
-		$polje = array('kod' => $status, 'poruka' => $poruka);
+		$polje = array('kod greske' => $status, 'poruka' => $poruka);
 		echo json_encode($polje);
 		$this->app->response->status($status);
 	}
 
 	// funkcija za ispis dohvacenih podataka iz baze podataka, konkretno ovdje se radi o dohvacenim korisnicima iz baze
+
 	protected function ispisDohvacenihPodataka($podaciZaIspis){
 		$ispis = array();
 
@@ -49,7 +48,7 @@ class Kontroler{
 				// ovo je dobar nacin za definiranje polja koje se sastoji od vise elemenata (vise od jednog elementa)
 				$pomocnoPolje[] = array('id' => $korisnik->id, 'Ime' => $korisnik->ime, 'Prezime' => $korisnik->prezime);
 			}
-			$ispis=array('korisnici'=>$pomocnoPolje);
+			$ispis = array('korisnici'=>$pomocnoPolje);
 
 		}
 		echo json_encode($ispis);
